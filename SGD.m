f17be07705c703f8627c18811e31ef98b187df8f
膜法师 <30488918+skywalker-young@users.noreply.github.com>
2017-12-31 00:16:34 +0800

    SGD，包含mini batch
    不同的梯度下降法，训练之前的backprop神经网络？？？目前怎么用还不是很明白。。。

---
+++ SGD.m
@@ -0,0 +1,28 @@
+function [] = SGD(training_data,test_data, epochs, eta_SGD)
+%        Train the neural network using full-batch stochastic
+%        gradient descent.  The ``training_data`` is a struct of arrays
+%        ``(x, y)`` representing the training inputs and the desired
+%        outputs. If ``test_data`` is provided then the
+%        network will be evaluated against the test data after each
+%        epoch, and partial progress printed out.  This is useful for
+%       tracking progress, but slows things down substantially."""
+
+        disp('After each iteration, results will be shown')     
+                 
+        global n_test_data = numel(test_data);
+        global num_layers = numel(sizes);
+        global psizes = sizes;
+        y=psizes(2:end);
+        global biases;
+        global weights;
+        global eta;
+        
+    for j =1:epochs                
+        update_mini_batch(training_data, eta_SGD);        
+     
+        
+            [n_test_score] = evaluate(test_data);        
+            fprintf('Epoch %d : %d out of %d \n', j, n_test_score, n_test_data)   
+    end
+
+end
